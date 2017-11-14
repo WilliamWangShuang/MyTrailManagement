11130f8c484f44a37c20d0485268c2a36349e49d
Shuang Wang <swan0005@student.monash.edu>
2017-11-13 21:04:57 -0800

    intial home page

--- MyProfileTrail/App_Start/BundleConfig.cs
+++ MyProfileTrail/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace MyProfileTrail
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/Style.css"));
         }
     }
 }
