36723bdd3b86b97044afc0c5d28c792c4b286a96
Shuang Wang <swan0005@student.monash.edu>
2017-11-15 03:48:22 -0800

    update register page

--- MyProfileTrail/App_Start/BundleConfig.cs
+++ MyProfileTrail/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace MyProfileTrail
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.min.js",
+                        "~/Scripts/FutureSpark.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,6 +27,9 @@ namespace MyProfileTrail
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery-ui.min.css",
+                      "~/Content/jquery-ui.structure.min.css",
+                      "~/Content/jquery-ui.theme.min.css",
                       "~/Content/site.css",
                       "~/Content/font-awesome.css",
                       "~/Content/Style.css"));
---
+++ MyProfileTrail/Scripts/FutureSpark.js
@@ -0,0 +1,16 @@
+﻿function register_Email_Tooltip() {
+    $('#register_email_tooltip').show();
+    $("#register_email_tooltip").delay(5000).hide();
+}
+
+$(document).ready(function () {
+    
+    var tooltips = $("[title]").tooltip({
+        position: {
+            my: "left top",
+            at: "right+5 top-5",
+            collision: "none"
+        }
+    });
+
+});
